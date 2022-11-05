52f49d31cc88fa0505d3d3e93a718bdd15cc72cb
korzepadawid <korzepadawid@yahoo.com>
2022-11-05 19:53:40 +0100

    feat: refactored to aws lamda

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from hackernews import fetch_main_page, parse_articles_page
 from twitter import merge_strings_within_chars_limit, TwitterClient
 
-def main() -> None:
+
+def lambda_handler(event, context) -> str:
     try:
         page = fetch_main_page()
         articles = [str(article) for article in parse_articles_page(page=page)]
         tweets = merge_strings_within_chars_limit(strs=articles)
         twitter = TwitterClient()
         twitter.post_sequentially(texts=tweets)
+        return "ok"
     except Exception as e:
-        print(e)
+        return f"an error occurred, {e}"
 
 
 if __name__ == "__main__":
-    main()
+    lambda_handler(event=None, context=None)
